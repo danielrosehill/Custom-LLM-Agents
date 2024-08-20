4cec757217633fcab67aea935b84858f8b38e480
Daniel Rosehill <daniel@danielrosehill.co.il>
2024-08-20 13:48:58 +0300

    updated

--- Exports/200824/convert.py
+++ Exports/200824/convert.py
@@ -1,6 +1,8 @@
 import pandas as pd
-file_path = 'GPTList.csv'
+
+file_path = 'list.csv'  
 df = pd.read_csv(file_path)
+
 
 markdown_file = 'list.md'
 
@@ -8,6 +10,7 @@ with open(markdown_file, 'w') as file:
     for index, row in df.iterrows():
         file.write(f"## {row['Name']}\n")
         file.write(f"{row['Summary']}\n\n")
-        file.write(f"[View GPT]({row['Link']})\n\n")
+        file.write(f"[View On ChatGPT]({row['Link']})\n\n")
+        file.write("---\n\n")
 
 print(f"Markdown file '{markdown_file}' has been created.")
