74f06e1bfaf3115e2e1d37ce538f024094889b23
Daniel Rosehill <daniel@danielrosehill.co.il>
2024-12-10 03:45:35 +0200

    updated

--- non-docs/scripts/list-creator.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aaada2b2b846f6032b937f3395979b4b076a09a96f7144cfb33886bc32cae9fd
-size 2619
---
+++ non-docs/scripts/list-creator/list-creator.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86a0abbb6d8326c3bb5b5467cfe8291c39c8abfec9fd6427ca051554f1dc0bc9
+size 2943
