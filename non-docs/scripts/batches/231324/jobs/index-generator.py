d7b2c542e5c2c66744aadbc41440b81f4dc3534c
Daniel Rosehill <daniel@danielrosehill.co.il>
2024-12-23 17:47:23 +0200

    updated

--- non-docs/scripts/batches/231324/jobs/index-generator.py
+++ non-docs/scripts/batches/231324/jobs/index-generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9393e2cc8b75efd42d60832e729411b8f7dc11e3346955f953066c9f7c23cde6
-size 3431
+oid sha256:a516d63b3cf4bc431da25e462246d19c182e3789410c1a9fe97199daea53a4cf
+size 3504
